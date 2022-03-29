dd3233452db37a9f688d8ad7cef280d0a24a4b8d
sanmofe <santiagomorafelix@gmail.com>
2022-03-29 16:33:28 -0500

    Está leyendo un archivo, acabo de notar que debemos leer el system.in lptm, igual se puede adaptar creo xD

--- src/ProblemaP1.java
+++ src/ProblemaP1.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ProblemaP1 {
@@ -8,13 +9,18 @@ public class ProblemaP1 {
         try {
             File archivo = new File("ESTO ES UN PLACEHOLDER");
             Scanner escaner = new Scanner(archivo);
-            int numCasos = escaner.nextInt();
-
-
-
-            while (escaner.hasNextLine()) {
-                String linea = escaner.nextLine();
-                System.out.println(linea);
+            escaner.nextLine(); //El número de casos nos vale vrg
+            String linea  = escaner.nextLine(); //Esto nos da los costos de los pisos del caso 1
+            while(!linea.isEmpty()) { //Vamos a recorrer todito el texto
+                String[] pesoPisos = linea.split(" "); //Ya dijimos que esto era el peso. salu2.
+                linea = escaner.nextLine();//Vamos a mirar los portales ahora
+                ArrayList<String[]> portales = new ArrayList<String[]>(); //Esto es lo más gracioso que he programado en mi vida xDDDD
+                while(linea.length() == 7){ //7 es el length de "W X Y Z", o sea, de un portal
+                    portales.add(linea.split(" ")); //Añadimos el portal a la lista de portales
+                    linea = escaner.nextLine(); //Y seguimos alv
+                }
+                //No hay necesidad de otro nextline pq lo hicimos al final del while de antes
+                iniciarCaso(pesoPisos, portales);
             }
             escaner.close();
         } catch (FileNotFoundException e) {
@@ -22,6 +28,11 @@ public class ProblemaP1 {
             e.printStackTrace();
         }
 
+    }
+
+    private static void iniciarCaso(String[] pesoPisos, ArrayList<String[]> portales) {
+        //TODO No lo sé, iniciar uno de los casos de prueba supongo xD
+        //Quizás sea bueno paralelizar esto
     }
 
     public void inicializar(){
