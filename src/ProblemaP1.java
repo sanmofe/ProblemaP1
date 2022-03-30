07d4072e4d6eea2b5eb8f09ae0f4e6fc78c97020
sanmofe <santiagomorafelix@gmail.com>
2022-03-29 22:26:56 -0500

    asd

--- src/ProblemaP1.java
+++ src/ProblemaP1.java
@@ -15,11 +15,11 @@ public class ProblemaP1 {
 			//FileReader read = new FileReader("./data/prueba");
 			BufferedReader br = new BufferedReader(in);
             String linea  = br.readLine(); //Esto nos da los costos de los pisos del caso 1
-            while(!linea.isEmpty()) { //Vamos a recorrer todito el texto
+            while(br.ready()) { //Vamos a recorrer todito el texto
                 String[] pesoPisos = linea.split(" "); //Ya dijimos que esto era el peso. salu2.
                 linea = br.readLine();//Vamos a mirar los portales ahora
                 ArrayList<String[]> portales = new ArrayList<String[]>(); //Esto es lo más gracioso que he programado en mi vida xDDDD
-                while(linea.length() == 7){ //7 es el length de "W X Y Z", o sea, de un portal
+                while(linea.length() == 7 && br.ready()){ //7 es el length de "W X Y Z", o sea, de un portal
                     portales.add(linea.split(" ")); //Añadimos el portal a la lista de portales
                     linea = br.readLine(); //Y seguimos alv
                 }
